d981677b82f37c8862c1bdd14f72fbc7da99ec5d
Dennis Tang <dennisvtang@gmail.com>
2024-03-24 01:59:37 -0700

    Use --force in push command
    For my use case I want to push files from one repo to another to a branch. I want the branch to always be the same and want the latest changes from the source repo. Currently if the branch exists and the action runs again it will fail.
    
    error: failed to push some refs to REPO
    hint: Updates were rejected because the remote contains work that you do not
    hint: have locally. This is usually caused by another repository pushing to
    hint: the same ref. If you want to integrate the remote changes, use
    hint: 'git pull' before pushing again.
    hint: See the 'Note about fast-forwards' in 'git push --help' for details.

--- entrypoint.sh
+++ entrypoint.sh
@@ -65,7 +65,7 @@ if git status | grep -q "Changes to be committed"
 then
   git commit --message "$INPUT_COMMIT_MESSAGE"
   echo "Pushing git commit"
-  git push -u origin HEAD:"$OUTPUT_BRANCH"
+  git push --force -u origin HEAD:"$OUTPUT_BRANCH"
 else
   echo "No changes detected"
 fi
