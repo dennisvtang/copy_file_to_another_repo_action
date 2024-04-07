686960df6ca10fc6b4d202ee4e5bfa2217cad410
Dennis Tang <dennisvtang@gmail.com>
2024-04-07 12:25:40 -0700

    Use -n instead of ! -z

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ git config --global user.name "$INPUT_USER_NAME"
 git config --global --add safe.directory '*'
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-if [ ! -z "$INPUT_RENAME" ]
+if [ -n "$INPUT_RENAME" ]
 then
   echo "Setting new filename: ${INPUT_RENAME}"
   DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_RENAME"
@@ -51,7 +51,7 @@ fi
 
 cd "$CLONE_DIR"
 
-if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
+if [ -n "$INPUT_DESTINATION_BRANCH_CREATE" ]
 then
   echo "Creating new branch: ${INPUT_DESTINATION_BRANCH_CREATE}"
   git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
@@ -69,13 +69,13 @@ if git status | grep -q "Changes to be committed"
 then
   git commit --message "$INPUT_COMMIT_MESSAGE"
 
-  if [ -z "$INPUT_USE_FORCE" ]
+  if [ -n "$INPUT_USE_FORCE" ]
   then
-    echo "Pushing git commit"
-    git push -u origin HEAD:"$OUTPUT_BRANCH"
-  else
     echo "Pushing git commit with --force"
     git push --force -u origin HEAD:"$OUTPUT_BRANCH"
+  else
+    echo "Pushing git commit"
+    git push -u origin HEAD:"$OUTPUT_BRANCH"
   fi
 
 else
